92cb3e6ff1e53d450c661e3453b7af10d702d2fa
aocattleya <aocattleya@yahoo.co.jp>
2018-06-06 17:16:04 +0900

    6/6 自宅学習用追加

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6b/c0e0713b2769001816ebf7e7820455c6
@@ -0,0 +1,15 @@
+package com.internousdev.webproj2.util;
+
+//	ここからimport
+
+
+public class DBConnector {
+	/**
+	 * JDBC ドライバー名
+	 */
+	private static String driverName = "com.mysql.jdbc.Driver";
+	/**
+	 * データベース接続 URL
+	 */
+
+}
--- WebProj2/src/com/internousdev/webproj2/util/DBConnector.java
+++ WebProj2/src/com/internousdev/webproj2/util/DBConnector.java
@@ -1,6 +1,8 @@
 package com.internousdev.webproj2.util;
 
-//	ここからimport
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 
 public class DBConnector {
@@ -11,5 +13,26 @@ public class DBConnector {
 	/**
 	 * データベース接続 URL
 	 */
+	private static String url = "jdbc:mysql://localhost/testdb";
+	/**
+	 * データベース接続ユーザ名
+	 */
+	private static String user = "root";
+	/**
+	 * データベース接続パスワード
+	 */
+	private static String password = "mysql";
 
+	public Connection getConnection(){
+		Connection con = null;
+		try{
+			Class.forName(driverName);
+			con = DriverManager.getConnection(url,user,password);
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
